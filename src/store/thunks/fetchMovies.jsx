c87881d7349a088e840d9086c77f05fc6b7cd9c2
Dikshit <dikshitwhite@gmail.com>
2023-12-13 10:58:30 +0530

    changed fetching location

--- src/store/thunks/fetchMovies.jsx
+++ src/store/thunks/fetchMovies.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const fetchMovies = createAsyncThunk("movies/fetch", async () => {
   const response = await axios.get(
-    "hhttps://moviedata-hpht.onrender.com/movies"
+    "https://moviedata-hpht.onrender.com/movies"
   );
 
   return response.data;
